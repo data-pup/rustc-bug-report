c10ab70e878f9aadb3d78f07f9b2b7e50f6c8755
data-pup <kevinmartinucr@gmail.com>
2018-03-31 09:58:48 -0700

    Renamed a variable in main.rs. This is not used elsewhere, we can just call it _ for the sake of dodging lint errors.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use lib::collect_args as collect_args_in_mod;
 
 fn main() {
     // Collect the arguments using the implementation in the helper module.
-    let other_args: Vec<String> = collect_args_in_mod();
+    let _ = collect_args_in_mod();
 }
 
