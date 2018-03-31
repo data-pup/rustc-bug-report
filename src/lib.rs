f93e9bb2ca3dbdd243a2945ac73802ceb4a90176
data-pup <kevinmartinucr@gmail.com>
2018-03-31 10:02:55 -0700

    Adjusting comments.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 /// function will not cause the same panic if it is moved to main.rs.
 pub fn foo() -> Vec<String> {
     std::env::args()
-        .skip(1)   // Compiler panics if this line is not commented out.
+        .skip(1)   // Compiler does not panic if this is commented out.
         .collect()
 }
 
