3e1eb90804764154c73498f01f284fd0061e81b3
data-pup <kevinmartinucr@gmail.com>
2018-03-31 09:57:31 -0700

    Identified bug scope, this is because the :: prefix is missing from the path.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 // use std; // No compile issue at all if this line is not commented out.
 
-/// This function will throw a cause the compiler to panic under certain
-/// if the standard library has not been imported into the module. This
-/// function will not cause the same panic if it is moved to main.rs.
+// /// This function will throw a cause the compiler to panic under certain
+// /// if the standard library has not been imported into the module. This
+// /// function will not cause the same panic if it is moved to main.rs.
+// pub fn collect_args() -> Vec<String> {
+//     std::env::args()
+//         .skip(1)   // Compiler panics if this line is not commented out.
+//         .collect()
+// }
+
+
+/// This is the same function without the missing `::` prefix.
 pub fn collect_args() -> Vec<String> {
-    std::env::args()
+    ::std::env::args()
         .skip(1)   // Compiler panics if this line is not commented out.
         .collect()
 }
